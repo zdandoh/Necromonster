a094c3076509f40c98bd2335e4688c3a9e64b93c
Dan Doh <link.dohi@gmail.com>
2013-04-27 11:44:04 -0700

    Added Player Class, Created map loader
    Added simple map loading system, modulated a lot of code. Almost a
    rewrite.

--- Necromonster.py
+++ Necromonster.py
@@ -3,30 +3,76 @@ from pygame.locals import *
 import sys
 import random
 
+sys.path.append('class')
+
+import mapLoader
+
 pygame.init()
+
+class Player():
+	def __init__(self, game):
+		self.game = game
+		self.player = pygame.image.load('rec/char/back1.png')
+		self.player_face = 'back' # this is the part of the player that you see
+		self.player_state = 1.
+		self.player_r = self.player.get_rect()
+
+		self.player_r.x = 50
+		self.player_r.y = 50
+
+	def update(self):
+		total_moved = [0, 0]
+		if 1 in self.game.keys_pressed:
+			if self.game.keys_pressed[K_w]:
+				self.player_r.y += -2
+				for rect in self.game.solid_list:
+					if self.player_r.colliderect(rect): self.player_r.y -= -2
+				self.player_face = 'back'
+			if self.game.keys_pressed[K_a]:
+				self.player_r.x += -2
+				for rect in self.game.solid_list:
+					if self.player_r.colliderect(rect): self.player_r.x -= -2
+				self.player_face = 'left'
+			if self.game.keys_pressed[K_s]:
+				self.player_r.y += 2
+				for rect in self.game.solid_list:
+					if self.player_r.colliderect(rect): self.player_r.y -= 2
+				self.player_face = 'front'
+			if self.game.keys_pressed[K_d]:
+				self.player_r.x += 2
+				for rect in self.game.solid_list:
+					if self.player_r.colliderect(rect): self.player_r.x -= 2
+				self.player_face = 'right'
+
+			self.player_state += 0.3
+			if self.player_state >= 4:
+				self.player_state = 1
+			self.player = pygame.image.load('rec/char/%s%s.png' % (self.player_face, int(self.player_state)))
+		if not self.game.keys_pressed[K_w] and not self.game.keys_pressed[K_a] and not self.game.keys_pressed[K_s] and not self.game.keys_pressed[K_d]:
+				self.player_state = 1
+
+	def addPos(self, move):
+		self.player_r.x += move[0]
+		self.player_r.y += move[1]
+
+	def setPos(self, new):
+		self.player_r.x = new[0]
+		self.player_r.y = new[1]
+
+	def blitPlayer(self):
+		self.game.screen.blit(self.player, (self.player_r.x, self.player_r.y))
+
 
 class Necro():
 	def __init__(self):
+		self.Player = Player(self)
 		# initiate the clock and screen
 		self.clock = pygame.time.Clock()
 		self.last_tick = pygame.time.get_ticks()
 		self.screen = pygame.display.set_mode([900, 650], 0, 32)
 
-		# player setup
-		self.player_face = 'back' # this is the part of the player that you see
-		self.player_state = 1.
-
-		# load all images
-		self.bg = pygame.image.load('rec/map/tower_bg.png').convert()
-		self.tower = pygame.image.load('rec/map/tower.png').convert_alpha()
-		self.player = pygame.image.load('rec/char/front1.png')
-
-		# load solids
-		self.tower_solid = pygame.image.load('rec/map/solids/tower_solid.png').convert_alpha()
-
-		# get rect for images
-		self.tower_r = self.tower_solid.get_rect()
-		self.player_r = self.player.get_rect()
+		# get the map that you are on
+		self.blit_list = mapLoader.load('home', self)
 
 		while 1:
 			self.Loop()
@@ -47,47 +93,21 @@ class Necro():
 
 	def Tick(self):
 		# updates to player location and animation frame
+		self.keys_pressed = pygame.key.get_pressed()
 		self.clock.tick()
-		keys_pressed = pygame.key.get_pressed()
-		total_moved = [0, 0]
-		if 1 in keys_pressed:
-			if keys_pressed[K_w]:
-				self.player_r.y += -2
-				if self.player_r.colliderect(self.tower_r): self.player_r.y -= -2
-				self.player_face = 'back'
-			if keys_pressed[K_a]:
-				self.player_r.x += -2
-				if self.player_r.colliderect(self.tower_r): self.player_r.x -= -2
-				self.player_face = 'left'
-			if keys_pressed[K_s]:
-				self.player_r.y += 2
-				if self.player_r.colliderect(self.tower_r): self.player_r.y -= 2
-				self.player_face = 'front'
-			if keys_pressed[K_d]:
-				self.player_r.x += 2
-				if self.player_r.colliderect(self.tower_r): self.player_r.x -= 2
-				self.player_face = 'right'
-
-			self.player_state += 0.3
-			if self.player_state >= 4:
-				self.player_state = 1
-			self.player = pygame.image.load('rec/char/%s%s.png' % (self.player_face, int(self.player_state)))
-		if not keys_pressed[K_w] and not keys_pressed[K_a] and not keys_pressed[K_s] and not keys_pressed[K_d]:
-				self.player_state = 1
+		self.Player.update()
 
 		self.last_tick = pygame.time.get_ticks()
 
 	def Draw(self):
-		# draw map image
-		self.screen.blit(self.bg, [0, 0])
-
-		# draw all solids
-		self.tower_r = self.screen.blit(self.tower_solid, [445, 384])
-
-		# draw player
-		self.player_r = self.screen.blit(self.player, (self.player_r.x, self.player_r.y))
-
-		# draw building images
-		self.screen.blit(self.tower, [440, 264])
+		solid_count = 0
+		for surf in self.blit_list:
+			if 'player' in surf:
+				self.Player.blitPlayer()
+			elif 'solid' in surf:
+				self.solid_list[solid_count] = self.screen.blit(surf[0], surf[1])
+				solid_count += 1
+			else:
+				self.screen.blit(surf[0], surf[1])
 
 Necro()
---
+++ class/mapLoader.py
@@ -0,0 +1,35 @@
+from pygame.image import load as imageload
+import os, random
+
+def load(map_name, game):
+	surfaces = []
+	game.solid_list = []
+	l = os.path.abspath(__file__).split('\\')
+	l.pop()
+	l.pop()
+	main_direc = '\\'.join(l) + '\\rec\\maps\\%s\\' % map_name
+	
+	# load bg
+	surfaces.append([imageload(main_direc + 'bg.png').convert(), [0, 0], 1])
+	# get dict from positions.txt
+	pos_dict = {}
+	positions = open(main_direc + 'positions.txt', 'r').read()
+	for line in positions.split('\n'):
+		if not line:
+			pass
+		elif line.startswith('#'):
+			pass
+		else:
+			ln = line.split(':')
+			pos_dict[ln[0]] = eval(ln[1]) # this is very dangerous. get less lazy and remove.
+	# load all solids
+	for fi in os.listdir(main_direc + 'solids\\'):
+		ident = random.randrange(1, 500000)
+		load_result = imageload(main_direc + 'solids\\' + fi).convert_alpha()
+		surfaces.append([load_result, pos_dict[fi], 'solid']) # the list format of a solid: [surface, position, type identifier]
+		game.solid_list.append(game.screen.blit(load_result, pos_dict[fi]))
+	surfaces.append('player')
+	# load all buildings
+	for fi in os.listdir(main_direc + 'build\\'):
+		surfaces.append([imageload(main_direc + 'build\\' + fi).convert_alpha(), pos_dict[fi], 3])
+	return surfaces
